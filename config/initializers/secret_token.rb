02001bb04f8a56d3fac7936fd404b97bc934e622
Cheng Chen <mr.chengchen@gmail.com>
2011-11-27 15:32:10 -0500

    First commit (:confirm => does not work)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '13f2870a5fd4551a671a350a34c05186276cdc1e9f4b8ff047255703f60d240680a627b1f51a5346c71f48f9e7230966b5ef8497037845d62876ccbf84831137'
